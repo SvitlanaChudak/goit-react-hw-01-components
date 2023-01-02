f6c0e208353e7706e3cf3bb69b5adbabd82a1d60
SvitlanaChudak <svit.lemon@gmail.com>
2023-01-02 17:19:31 +0200

    Add TransactionHistory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from '../../src/user.json';
 import data from '../../src/data.json';
 import friends from '../../src/friends.json';
+import transactions from '../../src/transactions.json';
 
 export const App = () => {
   return (
@@ -26,6 +28,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
