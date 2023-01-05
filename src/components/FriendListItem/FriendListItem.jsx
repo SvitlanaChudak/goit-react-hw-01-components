79dff2f26a18bfffbf5968ebebff88800c02c1a0
SvitlanaChudak <svit.lemon@gmail.com>
2023-01-05 11:19:53 +0200

    Add data folder, update css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,8 +3,7 @@ import css from './FriendListItem.module.css'
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return <li className={css.item}>
-    {isOnline ? (<span className={css.isOnline}></span>) :
-      (<span className={css.isOffline}></span> )}
+  <span className={isOnline ? css.isOnline : css.isOffline}></span>
   <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={css.name}>{name}</p>
 </li>
